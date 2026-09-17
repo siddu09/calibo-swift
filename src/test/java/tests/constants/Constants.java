684c606b8a9304e2b1eb8cdd05bff2c338b51308
Siddesh Belura <siddeshb400@gmail.com>
2026-09-17 19:05:35 +0530

    added test tags

--- src/test/java/tests/constants/Constants.java
+++ src/test/java/tests/constants/Constants.java
@@ -3,9 +3,13 @@ package tests.constants;
 public final class Constants {
 
     private Constants() {
-        // Prevent instantiation
+
     }
 
     public static final String PRO_REGRESSION = "PRO_Regression";
+    public static final String PRO_SANITY = "PRO_Sanity";
+    public static final String PRO_SMOKE = "PRO_Smoke";
+    public static final String PRO_PORTFOLIO = "PRO_Portfollio";
+    public static final String PRO_PRODUCT = "PRO_Product";
     public static final String PRO_E2E = "PRO_E2E";
 }
--- src/test/java/tests/ui/PRO/PortfolioTest.java
+++ src/test/java/tests/ui/PRO/PortfolioTest.java
@@ -13,13 +13,11 @@ import tests.constants.Constants;
 import utils.LoggerUtil;
 
 public class PortfolioTest extends BaseUITest {
-
     private ProductPortfolioFlows portfolioFlows;
     private ProExecutionData executionData;
 
-
     @BeforeMethod
-    @Step("Setup: Login and initialize ProductPortfolioFlows")
+    @Step("Setup: Login and Initialize ProductPortfolioFlows")
     public void setUp() {
         LoginBuildingBlock loginBuildingBlock = new LoginBuildingBlock(page);
         loginBuildingBlock.proLogin();
@@ -27,98 +25,85 @@ public class PortfolioTest extends BaseUITest {
         portfolioFlows = new ProductPortfolioFlows(page, executionData);
         LoggerUtil.LOGGER.info("============================ Setup completed =========================");
     }
-
-
-    @Test(groups = {Constants.PRO_REGRESSION})
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
     public void createPrivatePortfolioWithMandatoryFields() {
         portfolioFlows.createPortfolioWithMandatoryFields();
-        LoggerUtil.LOGGER.info("[PRO-E2E-STEP] Recording execution results...");
-        ProExecutionResultWriter.write("ProE2E", "Pro E2E With Mandatory Fields", executionData);
+        ProExecutionResultWriter.write("ProE2E", "createPrivatePortfolioWithMandatoryFields", executionData);
         portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "Pro E2E With Mandatory Fields");
-   }
-    @Test(groups = {Constants.PRO_REGRESSION})
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
     public void cancelPortfolioCreation() {
         portfolioFlows.cancelPortfolioCreation();
         ProExecutionResultWriter.write("ProE2E", "cancelPortfolioCreation", executionData);
-   }
-    @Test(groups = {Constants.PRO_REGRESSION})
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
     public void createPublicPortfolioWithAllFields() {
         portfolioFlows.createPublicPortfolioWithAllFields();
         ProExecutionResultWriter.write("ProE2E", "createPublicPortfolioWithAllFields", executionData);
         portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "createPublicPortfolioWithAllFields");
-}
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void addMultipleYearFinancials () {
-            portfolioFlows.addMultipleYearFinancials();
-            ProExecutionResultWriter.write("ProE2E", "addMultipleYearFinancials", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "addMultipleYearFinancials");
-        }
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateGloballyConfiguredCustomFields () {
-            portfolioFlows.validateGloballyConfiguredCustomFields();
-            ProExecutionResultWriter.write("ProE2E", "validateGloballyConfiguredCustomFields", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateGloballyConfiguredCustomFields");
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void editExistingPortfolio () {
-            portfolioFlows.editExistingPortfolio();
-            ProExecutionResultWriter.write("ProE2E", "editExistingPortfolio", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "editExistingPortfolio");
-        }
-//
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void editWorkflowTemplate () {
-            portfolioFlows.editWorkflowTemplate();
-            ProExecutionResultWriter.write("ProE2E", "editWorkflowTemplate", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "editWorkflowTemplate");
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateMissingMandatoryFieldsDuringCreation () {
-            portfolioFlows.validateMissingMandatoryFields();
-            ProExecutionResultWriter.write("ProE2E", "validateMissingMandatoryFieldsDuringCreation", executionData);
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateMissingOwnerAndWorkflowFields () {
-            portfolioFlows.validateMissingOwnerAndWorkflowFields();
-            ProExecutionResultWriter.write("ProE2E", "validateMissingOwnerAndWorkflowFields", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateMissingOwnerAndWorkflowFields");
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateInvalidLogoFormat () {
-            portfolioFlows.validateInvalidLogoFormat();
-            ProExecutionResultWriter.write("ProE2E", "validateInvalidLogoFormat", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateInvalidLogoFormat");
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateMyProductPortfolios () {
-            portfolioFlows.validateMyProductPortfolios();
-            ProExecutionResultWriter.write("ProE2E", "validateMyProductPortfolios", executionData);
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateAllProductPortfolios () {
-            portfolioFlows.validateAllProductPortfolios();
-            ProExecutionResultWriter.write("ProE2E", "validateAllProductPortfolios", executionData);
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void validateAuditHistorySearchAndFilters () {
-            portfolioFlows.validateAuditHistorySearchAndFilters();
-            ProExecutionResultWriter.write("ProE2E", "validateAuditHistorySearchAndFilters", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateAuditHistorySearchAndFilters");
-        }
-
-        @Test(groups = {Constants.PRO_REGRESSION})
-        public void downloadAuditHistoryInPdfAndCsvFormats () {
-            portfolioFlows.downloadAuditHistoryInPdfAndCsvFormats();
-            ProExecutionResultWriter.write("ProE2E", "downloadAuditHistoryInPdfAndCsvFormats", executionData);
-            portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "downloadAuditHistoryInPdfAndCsvFormats");
-        }
-
     }
-
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void addMultipleYearFinancials () {
+        portfolioFlows.addMultipleYearFinancials();
+        ProExecutionResultWriter.write("ProE2E", "addMultipleYearFinancials", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "addMultipleYearFinancials");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateGloballyConfiguredCustomFields () {
+        portfolioFlows.validateGloballyConfiguredCustomFields();
+        ProExecutionResultWriter.write("ProE2E", "validateGloballyConfiguredCustomFields", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateGloballyConfiguredCustomFields");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void editExistingPortfolio () {
+        portfolioFlows.editExistingPortfolio();
+        ProExecutionResultWriter.write("ProE2E", "editExistingPortfolio", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "editExistingPortfolio");
+    }
+    //
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void editWorkflowTemplate () {
+        portfolioFlows.editWorkflowTemplate();
+        ProExecutionResultWriter.write("ProE2E", "editWorkflowTemplate", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "editWorkflowTemplate");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateMissingMandatoryFieldsDuringCreation () {
+        portfolioFlows.validateMissingMandatoryFields();
+        ProExecutionResultWriter.write("ProE2E", "validateMissingMandatoryFieldsDuringCreation", executionData);
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateMissingOwnerAndWorkflowFields () {
+        portfolioFlows.validateMissingOwnerAndWorkflowFields();
+        ProExecutionResultWriter.write("ProE2E", "validateMissingOwnerAndWorkflowFields", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateMissingOwnerAndWorkflowFields");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateInvalidLogoFormat () {
+        portfolioFlows.validateInvalidLogoFormat();
+        ProExecutionResultWriter.write("ProE2E", "validateInvalidLogoFormat", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateInvalidLogoFormat");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateMyProductPortfolios () {
+        portfolioFlows.validateMyProductPortfolios();
+        ProExecutionResultWriter.write("ProE2E", "validateMyProductPortfolios", executionData);
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateAllProductPortfolios () {
+        portfolioFlows.validateAllProductPortfolios();
+        ProExecutionResultWriter.write("ProE2E", "validateAllProductPortfolios", executionData);
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void validateAuditHistorySearchAndFilters () {
+        portfolioFlows.validateAuditHistorySearchAndFilters();
+        ProExecutionResultWriter.write("ProE2E", "validateAuditHistorySearchAndFilters", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "validateAuditHistorySearchAndFilters");
+    }
+    @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
+    public void downloadAuditHistoryInPdfAndCsvFormats () {
+        portfolioFlows.downloadAuditHistoryInPdfAndCsvFormats();
+        ProExecutionResultWriter.write("ProE2E", "downloadAuditHistoryInPdfAndCsvFormats", executionData);
+        portfolioFlows.deletePortfolioWithoutProduct("ProE2E", "downloadAuditHistoryInPdfAndCsvFormats");
+    }
+}
