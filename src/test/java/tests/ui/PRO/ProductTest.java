b76d5669b72adce3cecefdc3fe10964d6a4bf893
Siddesh Belura <siddeshb400@gmail.com>
2026-09-21 14:50:46 +0530

    small change

--- src/test/java/tests/ui/PRO/ProductTest.java
+++ src/test/java/tests/ui/PRO/ProductTest.java
@@ -2,6 +2,7 @@ package tests.ui.PRO;
 
 import UI.E2E.LoginBuildingBlock;
 import UI.PRO.Product.Flows.ProductFlows;
+import UI.PRO.ProductPortfolio.Flows.ProductPortfolioFlows;
 import io.qameta.allure.Step;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,6 +16,7 @@ public class ProductTest extends BaseUITest {
 
     private ProductFlows productFlows;
     private ProExecutionData executionData;
+    private ProductPortfolioFlows portfolioFlows;
 
 
     @BeforeMethod
@@ -24,11 +26,13 @@ public class ProductTest extends BaseUITest {
         loginBuildingBlock.proLogin();
         executionData = new ProExecutionData();
         productFlows = new ProductFlows(page, executionData);
+        portfolioFlows = new ProductPortfolioFlows(page, executionData);
         LoggerUtil.LOGGER.info("============================ Login and Setup completed =========================");
     }
 
     @Test(groups = {Constants.PRO_REGRESSION,Constants.PRO_PORTFOLIO})
     public void createPrivateProductWithMandatoryFields() {
+        portfolioFlows.createPortfolioWithMandatoryFields();
         productFlows.createProductWithMandatoryFields();
         ProExecutionResultWriter.write("ProE2E", "createPrivateProductWithMandatoryFields", executionData);
 
